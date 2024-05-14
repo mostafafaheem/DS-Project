b9fe0f20f40e0341cd889aa59de88bc7fbf3d117
Mostafa Ahmad <mostafas.main.email@gmail.com>
2024-05-14 19:09:11 +0300

    malinka

--- globals.cpp
+++ globals.cpp
@@ -1,13 +1,13 @@
-#include<iostream>
 #include "trie.h"
 #include "models.h"
 #include "database.h"
 #include "search.h"
 #include "globals.h"
-using namespace std;
 
 Trie* trie = new Trie();
-vector<property> properties;
+vector<property> listings;
 vector<User*> usersList;
 User* currentLoggedIn = NULL;
 RBTree<float, Property> properties;
+static atomic<int> idCounter;
+
